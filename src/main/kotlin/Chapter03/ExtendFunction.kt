b07ad6b654e3a44c05529a49b0c9d75f6819fcb3
woo-chang <clllickme@naver.com>
2022-12-06 13:28:27 +0900

    확장 함수 예제 코드

---
+++ src/main/kotlin/Chapter03/ExtendFunction.kt
@@ -0,0 +1,41 @@
+package Chapter03
+
+fun main() {
+    val list = listOf(1, 2, 3)
+    println(list.joinToString())
+    val view: View = Button()
+    view.click()
+    view.showOff()
+}
+
+fun <T> Collection<T>.joinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun String.lastChar(): Char = this.get(this.length-1)
+
+val String.lastCharProperty: Char
+    get() = get(length - 1)
+
+open class View {
+    open fun click() = println("View clicked")
+}
+
+class Button: View() {
+    override fun click() = println("Button clicked")
+}
+
+fun View.showOff() = println("I'm a view!")
+fun Button.showOff() = println("I'm a button!")
